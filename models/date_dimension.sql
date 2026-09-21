7b6b38510c9ed4c84fb183587ae3f558482523e6
R R <rrivera100@hotmail.com>
2026-09-21 18:40:46 +0000

    date_dimension model

---
+++ models/date_dimension.sql
@@ -0,0 +1,24 @@
+with cte as
+(
+    select  to_timestamp(started_at) as started_at,
+            date(to_timestamp(started_at)) as date_started_at,
+            hour(to_timestamp(started_at)) as hour_started_at,
+            case
+                when dayname(to_timestamp(started_at)) in ('sat', 'sun')
+                then 'weekend'
+                else 'business day'
+            end as day_type,
+            case
+                when month(to_timestamp(started_at)) in (12,1,2)
+                then 'winter'
+                when month(to_timestamp(started_at)) in (3,4,5)
+                then 'spring'
+                when month(to_timestamp(started_at)) in (6,7,8)
+                then 'summer'
+                else 'fall'
+            end as season_of_year
+    from {{ source('demo', 'bike') }}
+    where started_at != 'started_at'
+)
+
+select * from cte
