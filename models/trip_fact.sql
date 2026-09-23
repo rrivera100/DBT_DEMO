72a050ad214b7fb19cb9595086c79ef0cd83a20d
R R <rrivera100@hotmail.com>
2026-09-23 17:27:46 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,10 @@
+with trips as
+(
+    select  ride_id, rideable_type, date(to_timestamp(started_at)) as trip_date,
+            start_station_id, end_station_id, member_casual,
+            timestampdiff(second, to_timestamp(started_at), to_timestamp(ended_at)) as trip_duration_seconds
+    from {{ source('demo', 'bike') }}
+    where ride_id != 'ride_id'
+)
+
+select * from trips
